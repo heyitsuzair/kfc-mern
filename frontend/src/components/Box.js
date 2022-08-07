e849168f0b1214d40afaa7803482407dae1a55e3
Muhammad Uzair <uzairdevil354123@gmail.com>
2022-08-07 21:54:25 +0500

    Top Bar Added

---
+++ frontend/src/components/Box.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Box } from "@mui/material";
+
+export default function Boxes() {
+  return (
+    <Box
+      sx={{
+        width: 28,
+        height: 14,
+        backgroundColor: "#E4002B",
+      }}
+    />
+  );
+}
---
+++ frontend/src/components/TopBar.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+import Box from "./Box";
+export default function TopBar() {
+  return (
+    <div className="top-bar">
+      <Box />
+      <Box />
+      <Box />
+      <Box />
+      <Box />
+    </div>
+  );
+}
