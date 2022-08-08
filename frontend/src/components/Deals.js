c235b4c03e796135e62fdcc551cff5230f4d2d91
Muhammad Uzair <uzairdevil354123@gmail.com>
2022-08-09 03:32:12 +0500

    Deals Section Added

---
+++ frontend/src/components/Deals.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function Deals({ src, name, classes }) {
+  console.log(classes);
+  return (
+    <div className="deals">
+      <div className="deals-inner">
+        <div
+          className={`deals-image ${classes === undefined ? "" : "active"}`}
+          style={{
+            border: src === "../images/deal1.png" ? "2px solid white" : "",
+          }}
+        >
+          <img src={src} width={70} alt="Deal" />
+        </div>
+        <strong>{name}</strong>
+      </div>
+    </div>
+  );
+}
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -4,7 +4,14 @@ import Header from "../components/Header";
 import TopBar from "../components/TopBar";
 import Hero from "../components/Hero";
 import Container from "@mui/material/Container";
+import Deals from "../components/Deals";
 
+import deal1 from "../images/deal1.png";
+import deal2 from "../images/deal2.png";
+import deal3 from "../images/deal3.png";
+import deal4 from "../images/deal4.png";
+import deal5 from "../images/deal5.png";
+import deal6 from "../images/deal6.png";
 export default function Home() {
   return (
     <>
@@ -12,6 +19,14 @@ export default function Home() {
       <Header />
       <Container>
         <Hero />
+        <div className="deal-container">
+          <Deals classes={"active"} src={deal1} name="Everyday Value" />
+          <Deals src={deal2} name="Ala-carte & Combos" />
+          <Deals src={deal3} name="Signature Boxes" />
+          <Deals src={deal4} name="Sharing" />
+          <Deals src={deal5} name="Snacks & Beverages" />
+          <Deals src={deal6} name="Midnight" />
+        </div>
       </Container>
     </>
   );
