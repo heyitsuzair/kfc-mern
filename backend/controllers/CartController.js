ec7641dd5989c6181d483eb8761e264dbc5ff5af
Muhammad Uzair <uzairdevil354123@gmail.com>
2022-08-23 21:47:52 +0500

    Get Cart Info Backend

--- backend/controllers/CartController.js
+++ backend/controllers/CartController.js
@@ -17,3 +17,17 @@ module.exports.addToCart = async (req, res) => {
     console.error(error);
   }
 };
+module.exports.getCartInfo = async (req, res) => {
+  try {
+    const { email } = req.params;
+    // count total items available in cart
+    const totalItems = await cart.find({ email }).count();
+
+    // get all items available in cart
+    const items = await cart.find({ email });
+
+    return res.status(200).json({ totalItems, items });
+  } catch (error) {
+    console.error(error);
+  }
+};
--- backend/routes/CartRoute.js
+++ backend/routes/CartRoute.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const { addToCart } = require("../controllers/CartController");
+const { addToCart, getCartInfo } = require("../controllers/CartController");
 router.post("/addToCart", addToCart);
+router.get("/getCartInfo/:email", getCartInfo);
 
 module.exports = router;
